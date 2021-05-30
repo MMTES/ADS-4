240a714f9bb3a7c8a88999c31e8cd5e07ceac7c3
MMTES <42440704+MMTES@users.noreply.github.com>
2021-05-30 23:41:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,7 +6,31 @@
 template<typename T>
 class TPQueue {
   // Сюда помещается описание структуры "Очередь с приоритетами"
+   private:
+    T* arr;
+    int size;
+    int begin, end;
+    int count;
+ public:
+    T pop();
+    T get() const;
+    bool isFull() const;
+    bool isEmpty() const;
 };
+
+template<typename T>
+T TPQueue<T>::get() const {
+    assert(count > 0);
+    return arr[begin];
+}
+template<typename T>
+bool TPQueue<T>::isEmpty() const {
+  return count == 0;
+}
+template<typename T>
+bool TPQueue<T>::isFull() const {
+  return count == size;
+}
 
 struct SYM {
   char ch;
